876399e3ca5666d335a18d51dcb60487c8ee9f27
tsunake <lpfxyww@163.com>
2018-02-09 17:01:57 +0800

    merge sort

---
+++ Simple/Simple/Simple/Common/Sort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.Common
+{
+    public class Sort
+    {
+        #region merge sort
+        public static void MergeSort<T>(List<T> src) where T : IComparable<T>
+        {
+            List<T> tmp = new List<T>(src.Count);
+            tmp.AddRange(Enumerable.Repeat<T>(default(T), src.Count));
+
+            MergeSort<T>(src, tmp, 0, src.Count - 1);
+        }
+
+        private static void MergeSort<T>(List<T> src, List<T> tmp, int left, int right) where T : IComparable<T>
+        {
+            if (left >= right)
+                return;
+            int mid = (left + right) / 2;
+            MergeSort<T>(src, tmp, left, mid);
+            MergeSort<T>(src, tmp, mid + 1, right);
+            Merge<T>(src, tmp, left, mid + 1, right);
+        }
+
+        private static void Merge<T>(List<T> src, List<T> tmp, int left_begin, int right_begin, int right_end) where T : IComparable<T>
+        {
+            int left_end = right_begin - 1;
+            int tmp_pos = left_begin;
+            int count = right_end - left_begin + 1;
+            while(left_begin <= left_end && right_begin <= right_end)
+            {
+                if (src[left_begin].CompareTo(src[right_begin]) < 0)
+                    tmp[tmp_pos++] = src[left_begin++];
+                else
+                    tmp[tmp_pos++] = src[right_begin++];
+            }
+            while(left_begin <= left_end)
+                tmp[tmp_pos++] = src[left_begin++];
+            while(right_begin <= right_end)
+                tmp[tmp_pos++] = src[right_begin++];
+            for (int i = 0; i < count; ++i, right_end--)
+                src[right_end] = tmp[right_end];
+        }
+        #endregion
+    }
+}
